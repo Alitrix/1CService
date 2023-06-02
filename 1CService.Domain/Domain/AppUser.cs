b97584d06ead777c86de71680327794745a90bcb
Alitrix <alien-kms@mail.ru>
2023-06-02 16:09:55 +1000

    Created class model application user

---
+++ 1CService.Domain/Domain/AppUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _1CService.Domain.Enums;
+
+namespace _1CService.Domain.Domain
+{
+    public class AppUser
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public WorkPlace WorkPlace { get; set; } = WorkPlace.None;
+        public string UserName { get; set; } = string.Empty;
+    }
+}
